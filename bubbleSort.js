2737187093c261ffb6d39b03c840c21f6f62f94f
Leo Curdi <ldcurdi@icloud.com>
2023-08-01 21:16:29 -0700

    optimized bubblesort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,8 +1,9 @@
 
-// Bubble sort algorithm
+// Bubble sort algorithm (optimized)
 async function bubbleSortAlgo() {
+    let noSwaps // this valirable will store whether at least 1 swap has been performed in a pass
     for (let i = 0; i < barArray.length - 1; i++) {
-
+        noSwaps = true // default it to true
         for (let j = 0; j < barArray.length - i - 1; j++) {
             barArray[j].classList.add("activeBar");
             barArray[j + 1].classList.add("activeBar");
@@ -28,6 +29,7 @@ async function bubbleSortAlgo() {
             if (integerArray[j] > integerArray[j + 1]) {
                 swapBars(j, j + 1);
                 swapInts(j, j + 1);
+                noSwaps = false // if a swap has been performed
             }
 
             await new Promise(resolve => setTimeout(() => { resolve(); }, interval));
@@ -37,6 +39,15 @@ async function bubbleSortAlgo() {
         }
         //await new Promise(resolve => setTimeout(() => { resolve(); }, interval));
         barArray[barArray.length - i - 1].classList.add("sortedBar");
+        
+        // if no swaps were performed, we know the array is sorted, so finish early
+        if (noSwaps) {
+            // mark all remaining bars as sorted
+            for (let k = barArray.length - i - 2; k >= 0; k--) {
+                barArray[k].classList.add('sortedBar')
+            }
+            break
+        }
     }
     barArray[0].classList.add("sortedBar"); // this line sets the leftmost bar to sorted at the end of the algorithm
 }
